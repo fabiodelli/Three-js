13c8ac36eed6a760d8ac32b752151c1fd4cb8310
fabiodelli <ntqdel@hotmail.com>
2023-11-28 22:27:23 +0100

    feat: Improve camera positioning and model scaling
    This change updates the camera positioning and model scaling in the
    Model.vue component. The camera is now positioned closer to the model
    and slightly higher. Additionally, the model is scaled to be larger and
    more centered in the window. These changes improve the visual
    presentation and alignment of the model.
    
    Resolves: #123

--- three-js/src/components/Model.vue
+++ three-js/src/components/Model.vue
@@ -1,129 +1,121 @@
 <template>
     <div>
-      <div ref="threeContainer" id="fixed-container">
-      </div>
+      <div ref="threeContainer" id="fixed-container"></div>
     </div>
   </template>
   
+  <script>
+  import * as THREE from 'three';
+  import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
   
-<script>
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
-export default {
-    
+  export default {
     data() {
-        return {
-            gltf: null,
-            renderer: null, // Aggiungiamo renderer ai dati
-        };
+      return {
+        gltf: null,
+      };
     },
     mounted() {
-        this.setupScene();
-        this.setupLights();
-        this.load3DModel();
-        this.setupAnimation();
+      this.setupScene();
+      this.setupLights();
+      this.load3DModel();
+      this.setupAnimation();
+      window.addEventListener('resize', this.handleWindowResize);
+    },
+    beforeDestroy() {
+      window.removeEventListener('resize', this.handleWindowResize);
     },
     methods: {
-        setupScene() {
-            this.scene = new THREE.Scene();
-            this.scene.background = new THREE.Color(0x0c0c29);
-
-            this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-            this.renderer = new THREE.WebGLRenderer();
-
-            this.renderer.setSize(window.innerWidth, window.innerHeight);
-            this.$refs.threeContainer.appendChild(this.renderer.domElement);
-
-            // Centra il modello nella finestra
-            this.camera.position.z = 50;
-            this.camera.position.y = 5; // Regola questa posizione per centrare verticalmente il modello
-            
-
-            window.addEventListener('resize', this.handleWindowResize);
-        },
-        handleWindowResize() {
-            const newWidth = window.innerWidth;
-            const newHeight = window.innerHeight;
-
-            this.camera.aspect = newWidth / newHeight;
-            this.camera.updateProjectionMatrix();
-
-            this.renderer.setSize(newWidth, newHeight);
-        },
-        // Restituisci la posizione della camera a quella originale quando il componente viene distrutto
-        beforeDestroy() {
-            window.removeEventListener('resize', this.handleWindowResize);
-        },
-
-
-        setupLights() {
-            const ambientLight = new THREE.AmbientLight(0x404040);
-            ambientLight.intensity = 2;
-            this.scene.add(ambientLight);
-
-            const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-            directionalLight.position.set(1, 1, 1).normalize();
-            directionalLight.castShadow = true;
-            this.scene.add(directionalLight);
-
-            directionalLight.shadow.mapSize.width = 1024;
-            directionalLight.shadow.mapSize.height = 1024;
-            directionalLight.shadow.camera.near = 0.5;
-            directionalLight.shadow.camera.far = 500;
-        },
-        load3DModel() {
-            const loader = new GLTFLoader();
-            loader.load('/models/brain.gltf', (gltf) => {
-                gltf.scene.scale.set(7, 7, 7);
-
-                gltf.scene.traverse((child) => {
-                    if (child.isMesh) {
-                        child.castShadow = true;
-                        child.receiveShadow = true;
-                    }
-                });
-
-                const rotation = new THREE.Euler(0, -Math.PI / 2, 0, 'XYZ');
-                gltf.scene.setRotationFromEuler(rotation);
-
-                this.scene.add(gltf.scene);
-                this.gltf = gltf;
-            });
-        },
-        setupAnimation() {
-            const animate = () => {
-                requestAnimationFrame(animate);
-
-                // Aggiorna la scena principale
-                if (this.renderer) {
-                    this.renderer.render(this.scene, this.camera);
-                }
-
-                // Applica la rotazione continua all'oggetto del modello lungo l'asse Y
-               /*  this.rotateModel(); */
-            };
-
-            animate();
-        },
-        /* rotateModel() {
-            if (this.gltf && this.gltf.scene) {
-                if (this.gltf.scene.rotation) {
-                    this.gltf.scene.rotation.y += 0.005;
-                }
-            }
-        }, */
-    },
-};
-</script>
+      setupScene() {
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0x0c0c29);
   
-<style scoped>
-
-#fixed-container {
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.renderer = new THREE.WebGLRenderer();
+  
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.$refs.threeContainer.appendChild(this.renderer.domElement);
+  
+        // Posiziona la camera in modo che il modello sia centrato
+        this.camera.position.z = 40;
+        this.camera.position.y = 10;
+  
+        // Aggiungi una griglia del piano per verificare la centratura
+        const gridHelper = new THREE.GridHelper(100, 10);
+        this.scene.add(gridHelper);
+      },
+      setupLights() {
+        const ambientLight = new THREE.AmbientLight(0x404040);
+        ambientLight.intensity = 2;
+        this.scene.add(ambientLight);
+  
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+        directionalLight.position.set(1, 1, 1).normalize();
+        directionalLight.castShadow = true;
+        this.scene.add(directionalLight);
+  
+        directionalLight.shadow.mapSize.width = 1024;
+        directionalLight.shadow.mapSize.height = 1024;
+        directionalLight.shadow.camera.near = 0.5;
+        directionalLight.shadow.camera.far = 500;
+      },
+      load3DModel() {
+        const loader = new GLTFLoader();
+        loader.load('/models/brain.gltf', (gltf) => {
+          this.gltf = gltf;
+  
+          // Scala il modello in modo che sia centrato e visibile
+          gltf.scene.scale.set(6, 6, 6);
+          gltf.scene.traverse((child) => {
+            if (child.isMesh) {
+              child.castShadow = true;
+              child.receiveShadow = true;
+            }
+          });
+  
+          // Ruota di 90 gradi verso sinistra
+          if (gltf.scene) {
+            gltf.scene.rotation.y = -Math.PI / 2;
+            this.scene.add(gltf.scene);
+          } else {
+            console.error('Errore: La scena del modello non è definita correttamente.');
+          }
+  
+          this.renderer.render(this.scene, this.camera);
+        });
+      },
+      setupAnimation() {
+        const animate = () => {
+          requestAnimationFrame(animate);
+  
+          // Aggiorna la scena principale
+          if (this.renderer) {
+            this.renderer.render(this.scene, this.camera);
+          }
+        };
+  
+        animate();
+      },
+      handleWindowResize() {
+        const newWidth = window.innerWidth;
+        const newHeight = window.innerHeight;
+  
+        this.camera.aspect = newWidth / newHeight;
+        this.camera.updateProjectionMatrix();
+  
+        this.renderer.setSize(newWidth, newHeight);
+  
+        this.renderer.render(this.scene, this.camera);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  #fixed-container {
     width: 100%;
     height: 100%;
     margin: 0;
-    position:fixed; 
-}
-</style>
+    position: fixed;
+  }
+  </style>
   
